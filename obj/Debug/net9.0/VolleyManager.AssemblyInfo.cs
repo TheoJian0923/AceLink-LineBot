61f016ad438841c2429c3cff5add9cdaea2e74e8
TheoJian <theojian@TheodeMac-mini.local>
2026-03-02 15:32:10 +0800

    Update helpMsg

--- obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f65e87a34b27a8a7e474328fd1df005fedb0deb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc797ef1244ead490efdd19b08726408104d83a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
