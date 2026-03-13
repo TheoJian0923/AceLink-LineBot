42ba287b00e66d443814bad7d24bb26da63ac235
TheoJian <theojian@TheodeMac-mini.local>
2026-03-13 15:14:35 +0800

    Update Manager command

--- obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7117189aa3de95e5b2fa10d4fb08f64c6b13698d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b641591bf84af6d73a6042d6405e5ae6e79391")]
 [assembly: System.Reflection.AssemblyProductAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
