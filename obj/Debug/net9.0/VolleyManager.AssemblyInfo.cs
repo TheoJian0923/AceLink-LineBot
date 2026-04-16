41937310da0fbaafd3cc47476224dd6a7f714e18
TheoJian <theojian@TheodeMac-mini.local>
2026-04-16 16:42:55 +0800

    更新防盜系統訊息

--- obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/VolleyManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bfb9d3d21f61abe3bcd8867bbd6200a12d9ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ed94f876e3fa3f804e7e2d01edbcbe1e96d689")]
 [assembly: System.Reflection.AssemblyProductAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VolleyManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
