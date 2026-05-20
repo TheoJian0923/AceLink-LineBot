7f980f0ed22474c4229b8a333f4f4d5eb8bb98da
TheoJian <theojian@TheodeMac-mini.local>
2026-05-20 12:07:35 +0800

    查找可報名時間問題

--- Program.cs
+++ Program.cs
@@ -1167,8 +1167,8 @@ app.MapPost("/api/linebot", async (HttpContext context, ILineMessagingClient lin
                             if (action == "+")
                             {
                                 // 💡 核心時段攔截機制：非管理員且非開發者時，進行可報名區間檢核
-                                //if (!data.IsWithinRegistrationPeriod(out string formattedRange))//測試用
-                                if (!isAdmin && !isDeveloper && !data.IsWithinRegistrationPeriod(out string formattedRange))
+                                if (!data.IsWithinRegistrationPeriod(out string formattedRange))//測試用，會阻擋管理員與開發者
+                                //if (!isAdmin && !isDeveloper && !data.IsWithinRegistrationPeriod(out string formattedRange))
                                 {
                                     await lineClient.ReplyMessageAsync(replyToken, $"您好，現在非報名時間請在{formattedRange}時間段做報名動作，感謝您");
                                 }
